5ad632d72727a5282364e8ddaa71191664c960b6
Summit Sayem <102522076+Summit-Sayem@users.noreply.github.com>
2022-04-29 05:12:04 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,42 @@
+//Mobile Menu Toggle
+
+const menuButton = document.getElementById("menuButton");
+
+const menu = document.getElementById("menu");
+
+menuButton.addEventListener('click', function() {
+  menu.classList.toggle('menuMobile');
+  menuButton.classList.toggle('closeButton');
+
+  return;
+})
+
+//RSVP Button
+
+const attendingButton = document.getElementById("attending");
+const regretsButton = document.getElementById("regrets");
+
+
+
+//Countdown Timer
+
+var countDownDate = new Date("May 15, 2022 19:00:00").getTime();
+
+var x = setInterval(function() {
+  var now = new Date().getTime();
+
+  var distance = countDownDate - now;
+
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("timer").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "May 15, 2022 7 PM";
+  }
+}, 1000);
